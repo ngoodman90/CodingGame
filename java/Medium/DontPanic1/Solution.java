d4537d27c749c2ee6b9b849c9912497b5498cdf7
ngoodman90 <ngoodman90@gmail.com>
2016-11-23 21:02:56 +0200

    Dont Panic 1 update

--- java/Medium/DontPanic1/Solution.java
+++ java/Medium/DontPanic1/Solution.java
@@ -20,15 +20,8 @@ class Player {
         int nbElevators = in.nextInt(); // number of elevators
         int[] elevators = new int[nbFloors];
         
-        System.err.println("Number of floors: " + nbFloors);
-        System.err.println("Number of elevators: " + nbElevators);
-        
-        for (int i = 0; i < nbElevators; i++) {
-            int elevatorFloor = in.nextInt(); // floor on which this elevator is found
-            int elevatorPos = in.nextInt(); // position of the elevator on its floor
-            elevators[elevatorFloor] = elevatorPos;
-            System.err.println("elevatorFloor: " + elevatorFloor + ", elevatorPos: " + elevatorPos);
-        }
+        for (int i = 0; i < nbElevators; i++) 
+            elevators[in.nextInt()] = in.nextInt();
 
         // game loop
         while (true) {
@@ -36,21 +29,15 @@ class Player {
             int clonePos = in.nextInt(); // position of the leading clone on its floor
             int currentElevatorPos = 0;
             String direction = in.next(); // direction of the leading clone: LEFT or RIGHT
-            if (cloneFloor < 0)
-            {
-                System.out.println("WAIT"); // action: WAIT or BLOCK
-                continue;
-            }
-            else if (cloneFloor == exitFloor)
-                currentElevatorPos = exitPos;
-            else
-                currentElevatorPos = elevators[cloneFloor];
-                
-            // Write an action using System.out.println()
-            // To debug: System.err.println("Debug messages...");
             
-            if (clonePos < currentElevatorPos && direction.equals("LEFT") 
+            if (cloneFloor == exitFloor)
+                currentElevatorPos = exitPos;
+            else if (cloneFloor >= 0)
+                currentElevatorPos = elevators[cloneFloor];
+            
+            if ((clonePos < currentElevatorPos && direction.equals("LEFT") 
             || clonePos > currentElevatorPos && direction.equals("RIGHT"))
+            && cloneFloor >= 0)
                 System.out.println("BLOCK"); // action: WAIT or BLOCK
             else
                 System.out.println("WAIT"); // action: WAIT or BLOCK
