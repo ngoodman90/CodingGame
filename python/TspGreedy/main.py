765f194e1451bb41a1b68e0d8089046a7d357426
ngoodman90 <ngoodman90@gmail.com>
2020-03-29 14:44:49 +0300

    Travelling Salesman Problem Greedy Algorithm

---
+++ python/TspGreedy/main.py
@@ -0,0 +1,27 @@
+import math
+from typing import List, Tuple
+
+
+def distance(p1, p2):
+    return math.sqrt((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2)
+
+
+def tsp(points: List[Tuple[int, int]]) -> float:
+    total_distance = 0.0
+    first_point = curr_point = points.pop(0)
+    while points:
+        next_point = min(points, key=lambda p: distance(curr_point, p))
+        total_distance += distance(curr_point, next_point)
+        curr_point = next_point
+        points.remove(next_point)
+    total_distance += distance(curr_point, first_point)
+    return total_distance
+
+
+def main():
+    points = [tuple([int(j) for j in input().split()]) for _ in range(int(input()))]
+    print(round(tsp(points)))
+
+
+if __name__ == "__main__":
+    main()
