eea8d8fa7769f27aef4c8475d9c3141cb2d7ed29
ngoodman90 <ngoodman90@gmail.com>
2020-04-26 14:33:06 +0300

    Shorten code

--- python/LruCache/main.py
+++ python/LruCache/main.py
@@ -6,6 +6,20 @@ class Node:
         self.prev_node = prev_node
         self.next_node = next_node
 
+    def move_to_head(self, head, tail):
+        if self is head:
+            return head, tail
+
+        if self is tail:
+            tail = tail.prev_node
+            tail.next_node = None
+        else:
+            self.prev_node.next_node = self.next_node
+            self.next_node.prev_node = self.prev_node
+        self.next_node = head
+        head.prev_node = self
+        return self, tail
+
 
 class LRUCache:
     def __init__(self, capacity: int):
@@ -13,55 +27,36 @@ class LRUCache:
         self.cache_dict = dict()
         self.cache_size = 0
         self.head_node = None
-        self.last_node = None
+        self.tail_node = None
 
     def get(self, key: int) -> int:
         curr_node = self.cache_dict.get(key)
         if curr_node is None:
             return -1
-        if curr_node is not self.head_node:
-            if curr_node is self.last_node:
-                self.last_node.prev_node.next_node = None
-                self.last_node = self.last_node.prev_node
-            else:
-                curr_node.prev_node.next_node = curr_node.next_node
-                curr_node.next_node.prev_node = curr_node.prev_node
-            self.head_node.prev_node = curr_node
-            curr_node.next_node = self.head_node
-            self.head_node = curr_node
+        self.head_node, self.tail_node = curr_node.move_to_head(self.head_node, self.tail_node)
         return curr_node.value
 
     def put(self, key: int, value: int) -> None:
         curr_node = self.cache_dict.get(key)
         if curr_node is not None:
-            if curr_node is self.head_node and curr_node is self.last_node:
-                self.head_node = self.last_node = None
-            elif curr_node is self.head_node:
-                self.head_node = self.head_node.next_node
-                self.head_node.prev_node = None
-            elif curr_node is self.last_node:
-                self.last_node = self.last_node.prev_node
-                self.last_node.next_node = None
-            else:
-                curr_node.prev_node.next_node = curr_node.next_node
-                curr_node.next_node.prev_node = curr_node.prev_node
-            self.cache_size -= 1
-
-        new_node = Node(key, value)
-        self.cache_dict[key] = new_node
-
-        if self.cache_size == 0:
-            self.head_node = new_node
-            self.last_node = new_node
+            curr_node.value = value
+            self.head_node, self.tail_node = curr_node.move_to_head(self.head_node, self.tail_node)
         else:
-            new_node.next_node = self.head_node
-            self.head_node.prev_node = new_node
-            self.head_node = new_node
+            new_node = Node(key, value)
+            self.cache_dict[key] = new_node
 
-        self.cache_size += 1
+            if self.cache_size == 0:
+                self.head_node = new_node
+                self.tail_node = new_node
+            else:
+                new_node.next_node = self.head_node
+                self.head_node.prev_node = new_node
+                self.head_node = new_node
 
-        if self.cache_size > self.capacity:
-            self.cache_dict[self.last_node.key] = None
-            self.last_node = self.last_node.prev_node
-            self.last_node.next_node = None
-            self.cache_size -= 1
+            self.cache_size += 1
+
+            if self.cache_size > self.capacity:
+                self.cache_dict[self.tail_node.key] = None
+                self.tail_node = self.tail_node.prev_node
+                self.tail_node.next_node = None
+                self.cache_size -= 1
